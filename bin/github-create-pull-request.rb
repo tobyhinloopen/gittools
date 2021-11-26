01f70b6e0403d0032a11fcc8ad309e0da368b4ce
Toby Hinloopen <hinlomedia@gmail.com>
2021-11-26 13:26:32 +0100

    Remove title and whitespace

--- bin/github-create-pull-request.rb
+++ bin/github-create-pull-request.rb
@@ -3,6 +3,6 @@
 branch_name = `git rev-parse --abbrev-ref HEAD`
 exit 1 unless branch_name =~ /^\d+/
 issue_id = $~
-title = `gh issue view #{issue_id} | head -n 1`
+title = `gh issue view #{issue_id} | head -n 1`.gsub(/^title:\s+|\n|\r/, "")
 
 puts `gh pr create --title "Fix #{title}" --body "Closes ##{issue_id}"`
